73e6a518c3cb237034cdb79d7c72a22b24f9a1ae
mustafizur012 <mustafizurbot01@gmail.com>
2025-05-23 08:38:25 +0800

    Update config.js

--- config.js
+++ config.js
@@ -13,7 +13,7 @@ AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "false",
 // make true if you want auto reply on status 
 AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || "true",
 // make true if you want auto reply on status 
-AUTO_STATUS_MSG: process.env.AUTO_STATUS_MSG || "*SEEN YOUR STATUS BY TOHID_MD 🇮🇳*",
+AUTO_STATUS_MSG: process.env.AUTO_STATUS_MSG || "*SEEN YOUR STATUS BY MUSTAFIZUR_MD 🇮🇳*",
 // set the auto reply massage on status reply  
 WELCOME: process.env.WELCOME || "true",
 // true if want welcome and goodbye msg in groups    
@@ -23,13 +23,13 @@ ANTI_LINK: process.env.ANTI_LINK || "true",
 // make anti link true,false for groups 
 MENTION_REPLY: process.env.MENTION_REPLY || "false",
 // make true if want auto voice reply if someone menetion you 
-MENU_IMAGE_URL: process.env.MENU_IMAGE_URL || "https://i.ibb.co/4ZSYvPTq/lordali.jpg",
+MENU_IMAGE_URL: process.env.MENU_IMAGE_URL || "https://i.ibb.co/4ZSYvPTq/.20250414_231502~2.jpg",
 // add custom menu and mention reply image url
 PREFIX: process.env.PREFIX || ".",
 // add your prifix for bot   
-BOT_NAME: process.env.BOT_NAME || "TOHID_MD",
+BOT_NAME: process.env.BOT_NAME || "MUSTAFIZUR",
 // add bot namw here for menu
-STICKER_NAME: process.env.STICKER_NAME || "TOHID_MD",
+STICKER_NAME: process.env.STICKER_NAME || "MUSTAFIZUR",
 // type sticker pack name 
 CUSTOM_REACT: process.env.CUSTOM_REACT || "false",
 // make this true for custum emoji react    
@@ -37,15 +37,15 @@ CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️�
 // chose custom react emojis by yourself 
 DELETE_LINKS: process.env.DELETE_LINKS || "false",
 // automatic delete links witho remove member 
-OWNER_NUMBER: process.env.OWNER_NUMBER || "917849917350",
+OWNER_NUMBER: process.env.OWNER_NUMBER || "60195168673",
 // add your bot owner number
-OWNER_NAME: process.env.OWNER_NAME || "Mr Tohid",
+OWNER_NAME: process.env.OWNER_NAME || "MP MUSTAFIZUR",
 // add bot owner name
-DESCRIPTION: process.env.DESCRIPTION || "*©𝙿𝙾𝚆𝙴𝚁𝙴𝙳 𝙱𝚈 𝚃𝙾𝙷𝙸𝙳_𝙼𝙳*",
+DESCRIPTION: process.env.DESCRIPTION || "*©𝙿𝙾𝚆𝙴𝚁𝙴𝙳 𝙱𝚈 MUSTAFIZUR*",
 // add bot owner name    
-ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/4ZSYvPTq/lordali.jpg",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/20250414_231502~2.jpg",
 // add img for alive msg
-LIVE_MSG: process.env.LIVE_MSG || "> TOHID_MD BOT IS ALIVE*⚡",
+LIVE_MSG: process.env.LIVE_MSG || "> MUSTAFIZUR_MD BOT IS ALIVE*⚡",
 // add alive msg here 
 READ_MESSAGE: process.env.READ_MESSAGE || "false",
 // Turn true or false for automatic read msgs
